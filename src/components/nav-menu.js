a7e5d7d4f3ecb02187ef89b6e455d00d9c51a996
Nick Smith <nick.smith.tsa@gmail.com>
2020-09-02 23:51:10 +0100

    link additions

--- src/components/nav-menu.js
+++ src/components/nav-menu.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default class navBar extends React.Component {
   render() {
     return (
       <div id="navbar">
         <ul>
-          {this.props.navLinks.map((link) => <li><a href={link.href} key = {link.href} > {link.name} </a></li>)}
+          {this.props.navLinks.map((link) => <li><Link to={link.href} key = {link.href} > {link.name} </Link></li>)}
         </ul>
       </div>
     )
