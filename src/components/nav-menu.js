2a3a0465789a0ef09e2acc477354f3c381da9a35
Nick <nick.smith.tsa@gmail.com>
2020-09-03 22:12:01 +0100

    refactored component

--- src/components/nav-menu.js
+++ src/components/nav-menu.js
@@ -8,16 +8,17 @@ import {
 import {navLinks} from "../util/nav-links";
 
 export const NavMenu = () => {
+  const lis = navLinks.map(link => (
+        <li>
+          <Link to={link.href} key={link.href}>{link.name}</Link>
+        </li>
+        )
+      )
+
   return (
     <div id="navbar">
         <ul>
-          {navLinks.map(link => {
-            return (
-              <li>
-                <Link to={link.href} key={link.href}>{link.name}</Link>
-              </li>
-            )
-          })}
+          {lis}
         </ul>
       </div>
   )
