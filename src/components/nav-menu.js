9e7b1112fb2aac8933ecf26e37d3cec0ffd8df73
Nick <nick.smith.tsa@gmail.com>
2020-09-05 12:04:02 +0100

    nav menu class component for active class state prep

--- src/components/nav-menu.js
+++ src/components/nav-menu.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {Component} from "react";
 import {Link} from "react-router-dom";
 import {navLinks} from "../util/nav-links";
 
+export default class NavMenu extends React.Component {
+  render() {
+    const lis = navLinks.map(link => (
+      <li>
+        <Link to={link.href} key={link.href}>{link.name}</Link>
+      </li>
+      )
+    )
+
+    return (
+      <div id="navbar">
+        <ul>
+          {lis}
+        </ul>
+      </div>
+    )
+  }
+}
+
+{
+  /*
 export const NavMenu = () => {
   const lis = navLinks.map(link => (
         <li>
@@ -17,4 +38,6 @@ export const NavMenu = () => {
         </ul>
       </div>
   )
+}
+*/
 }
